d0d65a97ed351a3d9fc91dd450499114a49eec19
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-29 15:08:09 +0200

    try to copmleted task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,8 @@ const myInput = document.querySelector('#datetime-picker');
 const inputButton = document.querySelector('[data-start]');
 inputButton.setAttribute('disabled', 'true');
 
+let selectedDate = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -13,10 +15,47 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
-      alert('Please choose a date in the future');
+      window.alert('Please choose a date in the future');
+      inputButton.setAttribute('disabled', 'true');
       return;
     }
     inputButton.removeAttribute('disabled');
+    selectedDate = selectedDates[0].getTime();
+    console.log(selectedDate);
   },
 };
+
+console.log(selectedDate);
 const fp = flatpickr(myInput, options); // flatpickr
+const timeToEndAction = Date.now() + selectedDate;
+
+function getTimeToEndAction() {
+  setInterval(() => {
+    const time = Date.now();
+    const currentTime = timeToEndAction - time;
+    console.log(currentTime);
+    const timer = convertMs(currentTime);
+    console.log(timer);
+  }, 1000);
+}
+
+// getTimeToEndAction();
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
