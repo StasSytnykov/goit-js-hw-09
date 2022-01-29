143daa46dab50f82be4e149363918bccd3292fd6
Stas Sytnykov <stas82kak05@gmail.com>
2022-01-29 13:02:32 +0200

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timerId = null;
+let isActive = false;
+
+buttonStart.addEventListener('click', onGetRandomBackgroundColor);
+buttonStop.addEventListener('click', onStopChangeBackgroundColor);
+
+function onGetRandomBackgroundColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeBackgroundColor() {
+  clearInterval(timerId);
+}
